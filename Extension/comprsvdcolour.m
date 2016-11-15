da6482762e1257a3cc3d419a0689f57c393cb832
yaojie <limyaojie93@gmail.com>
2016-11-15 16:25:28 +0800

    chester idea

--- Extension/comprsvdcolour.m
+++ Extension/comprsvdcolour.m
@@ -1,7 +1,7 @@
 % compress an image using svd
 % convert color image to grayscale so we don't need to make 2 images
 
-pic = double(imread('test.jpg'));
+pic = double(imread('hatsune.png'));
 
 % split the image into 3 separate channels, r g b.
 r = pic(:,:,1);
@@ -11,12 +11,12 @@ b = pic(:,:,3);
 % csvd compress each color channel
 % based on color channel, individually optimize and compress
 figure;
-cr = csvd(double(r), 1, 'red');
-cg = csvd(double(g), 3, 'green');
-cb = csvd(double(b), 5, 'blue');
+cr = csvd(double(r));
+cg = csvd(double(g));
+cb = csvd(double(b));
 
 % concatenate the three channels together after compression
 final = cat(3, cr, cg, cb);
 
 % write compressed image to file
-imwrite(uint8(final), 'compressed.jpg');
+imwrite(uint8(final), 'compressed_hatsune.png');
